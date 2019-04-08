6cd75a192225941543750ca4d20482673068c924
Omar Shafik <o.shafik13@gmail.com>
2019-04-08 14:21:16 +0200

    proccess update

--- proccessClass.py
+++ proccessClass.py
@@ -1,8 +1,10 @@
 class Proccess:
     procCount = 0
 
-    def __init__(self, priority, arrival_t, duration):
+    def __init__(self, arrival_t, duration, priority):
         self.priority = priority
         self.arrival_t = arrival_t
         self.duration = duration
         Proccess.procCount += 1
+
+# an array of Proccess objects should be passed to each scheduler function
